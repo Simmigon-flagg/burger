44edeeddb1fd251c43b192bd2af664196e78e303
Simmigon <simmigon.flagg@gmail.com>
2018-10-13 15:30:53 -0400

    Updating controllers post methods and api methods

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,3 +4,43 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+  burger.all(function(data) {
+    var burgerObject = {
+      burgers: data
+    };
+    console.log(burgerObject);
+    res.render("index", burgerObject);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.create(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update(
+    {
+      devoured: req.body.devoured
+    },
+    condition,
+    function(result) {
+      if (result.changedRows === 0) {
+        // User Doesn't exist in database
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
+});
+
+// Export routes for server.js to use.
+module.exports = router;
