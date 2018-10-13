47c6ea6318cb28deb72a78c418db0be8ab4a794c
Simmigon <simmigon.flagg@gmail.com>
2018-10-13 15:22:34 -0400

    Added Connection DB in config

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "burgers_db"
+});
+
+module.exports =  connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
