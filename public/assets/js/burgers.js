989bd771d19744aa1fe1573d791d29915bf1904f
Simmigon <simmigon.flagg@gmail.com>
2018-10-14 16:39:05 -0400

    Added changes to fix the views

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -32,7 +32,7 @@ $(function() {
       burger_name: $("#bur").val().trim(),
       devoured: $("[name=devoured]:checked").val().trim()
     };
-    console.log(newBurger);
+  
     // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
